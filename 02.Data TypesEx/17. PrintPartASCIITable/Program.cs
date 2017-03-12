29c7323c886a5e82b2b1b3cf4a5c5971db993c72
KaraD7 <karaangelov.d@gmail.com>
2017-03-12 11:58:40 +0200

    update 03/12/2017 (1)

---
+++ 02.Data TypesEx/17. PrintPartASCIITable/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17.Print_Part_Of_ASCII_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOne = int.Parse(Console.ReadLine());
+            int numberTwo = int.Parse(Console.ReadLine());
+
+
+            for (int symbol = numberOne; symbol <= numberTwo; symbol++)
+            {
+                
+                char character = (char)symbol;
+                string text = character.ToString();
+                Console.Write($"{text} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
