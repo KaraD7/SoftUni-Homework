896558a395330b8d728da9a575b0e52a7fa17382
KaraD7 <karaangelov.d@gmail.com>
2017-03-14 22:15:11 +0200

    add Lab Dictionary

---
+++ 07.Dictionaries/00.LabExercise/Dictionaries.cs
@@ -0,0 +1,28 @@
+﻿namespace _00.LabExercise
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Dictionaries
+    {
+        public static void Main()
+        {
+            Dictionary <string, string> phoneBook = new Dictionary<string, string>();
+            phoneBook["John Smith"] = "";
+            phoneBook["Lisa"] = "0877655553555";
+            phoneBook["Sam Doe"] = "0877653555";
+            phoneBook["Nakov"] = "0877653555";
+            phoneBook["Nakov"] = "0877653555";
+
+
+            foreach (var item in phoneBook)
+            {
+                var key = item.Key;
+                var value = item.Value;
+                Console.WriteLine($"{key} --> {value}");
+            }
+
+
+        }
+    }
+}
---
+++ 07.Dictionaries/00.LabExercise2/Dictionaries.cs
@@ -0,0 +1,20 @@
+﻿namespace _00.LabExercise
+{
+    using System;
+    using System.Collections.Generic;
+    public class Dictionaries
+    {
+        public static void Main()
+        {
+            Dictionary<string, string> phoneBook = new Dictionary<string, string>();
+
+            phoneBook["nakov"] = "55555555";
+            phoneBook["misho"] = "6547787787878787";
+
+            foreach (var pair in phoneBook)
+            {
+                Console.WriteLine("{0} --> {1}", pair.Key, pair.Value);
+            }
+        }
+    }
+}
