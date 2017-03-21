b01f28658949a5d2ecf7f1553d05e2fc706215e7
KaraD7 <karaangelov.d@gmail.com>
2017-03-21 13:37:14 +0200

    add homework (Dictionaries)

--- 07.Dictionaries/00.LabExercise/Dictionaries.cs
+++ 07.Dictionaries/00.LabExercise/Dictionaries.cs
@@ -2,13 +2,14 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
 
     public class Dictionaries
     {
         public static void Main()
         {
             Dictionary <string, string> phoneBook = new Dictionary<string, string>();
-            phoneBook["John Smith"] = "";
+            phoneBook["John Smith"] = "558976";
             phoneBook["Lisa"] = "0877655553555";
             phoneBook["Sam Doe"] = "0877653555";
             phoneBook["Nakov"] = "0877653555";
@@ -21,8 +22,6 @@
                 var value = item.Value;
                 Console.WriteLine($"{key} --> {value}");
             }
-
-
         }
     }
 }
--- 07.Dictionaries/00LabPhonebook/Program.cs
+++ 07.Dictionaries/00LabPhonebook/Program.cs
@@ -1,27 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-public class Program
+﻿namespace _01.Letter_Repetition
 {
-    
-    public static void Main()
+    using System;
+    using System.Collections.Generic;
+    public class Program
     {
-        // Example Dictionary again.
-        Dictionary<string, int> d = new Dictionary<string, int>()
+        public static void Main()
         {
-            {"cat", 2},
-            {"dog", 1},
-            {"llama", 0},
-            {"iguana", -1}
-        };
-        // Loop over pairs with foreach.
-        foreach (KeyValuePair<string, int> pair in d)
-        {
-            Console.WriteLine("{0}, {1}", pair.Key, pair.Value);
-        }
-        // Use var keyword to enumerate dictionary.
-        foreach (var pair in d)
-        {
-            Console.WriteLine("{0}, {1}", pair.Key, pair.Value);
+            Dictionary<string, string> phoneBook = new Dictionary<string, string>();
+
+            phoneBook["John Smith"] = "558976";
+            phoneBook["Lisa"] = "0877655553555";
+            phoneBook["Sam Doe"] = "0877653555";
+            phoneBook["Nakov"] = "0877653555";
+            phoneBook["Nakov"] = "0877653555";
+
+            foreach (var item in phoneBook)
+            {
+                Console.WriteLine($"{item.Key} --> {item.Value}");
+            }
         }
     }
 }
