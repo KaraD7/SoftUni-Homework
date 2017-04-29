cfbd431af92f34e1701fabed7810f67e297665cf
KaraD7 <karaangelov.d@gmail.com>
2017-04-29 15:15:49 +0300

    Add exercises Exam I

---
+++ 01.Exam Preparation I TSE/04.WinningTicket/WinningTickets.cs
@@ -0,0 +1,57 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _04.WinningTicket
+{
+    using System;
+    using System.Linq;
+    public class WinningTickets
+    {
+        public static void Main()
+        {
+            const string pattern  = @"([$#^@])\1{5,}";
+
+            var ticketRegex = new Regex(pattern);
+
+            var tickets = Console.ReadLine()
+                .Split(',')
+                .Select(a => a.Trim())
+                .ToArray();
+
+            foreach (var ticket in tickets)
+            {
+                if (ticket.Length !=20)
+                {
+                    Console.WriteLine("invalid ticket");
+                    continue;
+                }
+
+                var leftHalf = ticket.Substring(0, ticket.Length / 2);
+                var rightHalf = ticket.Substring(ticket.Length / 2);
+
+                var leftMatch = ticketRegex.Match(leftHalf);
+                var rightMatch = ticketRegex.Match(rightHalf);
+
+                if (leftMatch.Success && rightMatch.Success)
+                {
+                    var winningSymbol = leftMatch.Value[0];
+                    var shortedMatch = Math.Min(leftMatch.Length, rightMatch.Length);
+
+                    var jackpot = string.Empty;
+
+                    if (shortedMatch == 10)
+                    {
+                        jackpot = "Jackpot!";
+                    }
+
+                    Console.WriteLine($"ticket \"{ticket}\" - {shortedMatch}{winningSymbol} {jackpot}");
+                }
+
+                else
+                {
+                    Console.WriteLine($"ticket \"{ticket}\" - no match");
+                }
+
+            }
+        }
+    }
+}
